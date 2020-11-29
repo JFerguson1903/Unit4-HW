747321d7a206210e514663490b1a2aad363915a5
Jessica Ferguson <jferguson1903@gmail.com>
2020-11-28 20:55:07 -0600

    reduce timer function added

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 var countdownDisplay = document.querySelector("#countdown");
-var secondsLeft;
-var secondsElapsed = 0;
 var startButton = document.querySelector("#start");
-var totalSeconds = 75;
+var wrongButton = document.querySelector("#wrong");
 
-function startTimer() {
+var totalSeconds = 0;
 
-    // We only want to start the timer if totalSeconds is > 0
-    if (totalSeconds > 0) {
-        /* The "interval" variable here using "setInterval()" begins the recurring increment of the
-           secondsElapsed variable which is used to check if the time is up */
-        interval = setInterval(function() {
-            secondsElapsed++;
-            totalSeconds--;
-            countdownDisplay.innerHTML = totalSeconds;
-        }, 1000);
-    } else {
-        alert("Minutes of work/rest must be greater than 0.")
-    }
+function quizStart() {
+    startTimer(76);
+
+
 }
 
-startButton.addEventListener("click", startTimer);
+function startTimer(updatedTime) {
+    totalSeconds = updatedTime;
+
+    interval = setInterval(function() {
+        totalSeconds--;
+        countdownDisplay.innerHTML = totalSeconds;
+    }, 1000);
+}
+
+function reduceTimer() {
+    clearInterval(interval);
+    var updatedTime = totalSeconds - 10;
+    startTimer(updatedTime);
+}
+
+startButton.addEventListener("click", quizStart);
+wrongButton.addEventListener("click", reduceTimer);
