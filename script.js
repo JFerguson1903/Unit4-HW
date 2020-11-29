df33c469f8a004b541939c20a84f199940b7547b
Jessica Ferguson <jferguson1903@gmail.com>
2020-11-29 15:33:59 -0600

    d-none container classes added

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var countdownDisplay = document.querySelector("#countdown");
 var startButton = document.querySelector("#start");
-var wrongButton = document.querySelector("#wrong");
-var quizTimeContainer = document.querySelector("#quizTime")
+var infoContainer = document.querySelector("#info");
+var quizTimeContainer = document.querySelector("#questions");
+var allDoneContainer = document.querySelector("#allDone");
+var quizTimeSection = document.querySelector("#quizTime");
+var finalScoreSpan = document.querySelector("#finalScore");
 var totalSeconds = 0;
 var activeQuestion = 0;
+var finalScore = 0;
 
 
 var questions = [{
@@ -11,69 +15,56 @@ var questions = [{
         choices: ["script", "scripting", "javascript", "js"],
         answer: 0
     },
-
     {
         question: "Where is the correct place to insert a JavaScript?",
         choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body> section"],
         answer: 1
     },
-
     {
         question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
         choices: ['script name="xxx.js"', 'script src="xxx.js"', 'script href="xxx.js"'],
         answer: 1
     }
-
 ];
 
-/* for (var i = 0; i < questions.length; i++) {
-    var question = questions[i].question;
-    quizTimeContainer.innerHTML = question;
-    document.write(question);
-    var options = questions[i].choices;
-    for (var opt in options) {
-
-        radioEle.value = options[opt];
-
-    }
-} */
-
-
+var questionsLength = questions.length - 1;
 
 function quizStart() {
+    infoContainer.classList.add("d-none");
     startTimer(76);
     loadQuestion(activeQuestion);
-    /*     for (var i = 0; i < questions.length; i++) {
-            var question = questions[i].question;
-            quizTimeContainer.innerHTML = question;
-            var options = questions[i].choices;
-            for (var opt in options) {
-                quizTimeContainer.innerHTML = options[opt];
-                console.log(options[opt]);
-            }
-        }
-     */
-
 }
 
 function loadQuestion(questionIndex) {
-    quizTimeContainer.innerHTML = `<h2>${questions[questionIndex].question}</h2>`;
+    quizTimeSection.innerHTML = `<h2>${questions[questionIndex].question}</h2>`;
 
     var options = questions[questionIndex].choices;
     for (var opt in options) {
-        quizTimeContainer.innerHTML += `<div><button type="button" value="${opt}" class="btnAnswer btn btn-secondary btn-sm my-2">${String(options[opt])}</button></div>`;
+        quizTimeSection.innerHTML += `<div><button type="button" value="${opt}" class="btnAnswer btn btn-secondary btn-sm my-2">${String(options[opt])}</button></div>`;
     }
 }
 
 function usersAnswerChoice(selectedAnswerIndex) {
-    if (selectedAnswerIndex == questions[activeQuestion].answer) {
+    if (selectedAnswerIndex === questions[activeQuestion].answer) {
         console.log("correct")
     } else {
         reduceTimer();
         console.log("you wrong SUCKAH!");
     };
-    activeQuestion++;
-    loadQuestion(activeQuestion);
+
+    if (activeQuestion === questionsLength) {
+        finalScore = totalSeconds;
+        finalScoreSpan.innerHTML = finalScore;
+        clearInterval(interval);
+        countdownDisplay.innerHTML = 0;
+        allDoneContainer.classList.remove("d-none");
+        quizTimeContainer.classList.add("d-none");
+
+    } else {
+        activeQuestion++;
+        loadQuestion(activeQuestion);
+    };
+
 }
 
 function startTimer(updatedTime) {
@@ -92,7 +83,6 @@ function reduceTimer() {
 }
 
 startButton.addEventListener("click", quizStart);
-wrongButton.addEventListener("click", reduceTimer);
 
 
 $(document.body).on('click', '.btnAnswer', function() {
