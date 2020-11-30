46fccb4b3a5e8d9e5d4022e7a899cda32ccf0aa5
Jessica Ferguson <jferguson1903@gmail.com>
2020-11-29 20:19:02 -0600

    made clear button operational

--- hsscript.js
+++ hsscript.js
@@ -3,6 +3,7 @@ var url = new URL(url_string);
 var initials = url.searchParams.get("initials");
 var score = url.searchParams.get("reportedScore");
 var listedHighScores = document.querySelector("#listedHighScores");
+var clearButton = document.querySelector("#clear");
 
 var results = [
     [initials, score]
@@ -13,13 +14,16 @@ localStorage.setItem("highScores", JSON.stringify(results));
 
 var currentHighScore = JSON.parse(localStorage.getItem("highScores"));
 
+var rank = 1;
+
 for (let index = 0; index < currentHighScore.length; index++) {
 
-    listedHighScores.innerHTML = `<li class="list-group-item list-group-item-secondary text-left">${currentHighScore[index][1]}</li>`
-
-    //console.log(currentHighScore[index][1]);
+    listedHighScores.innerHTML = `<li class="list-group-item list-group-item-secondary text-left">${rank}. ${currentHighScore[index][0]} - ${currentHighScore[index][1]}</li>`
 }
 
-function name(params) {
-
+function clearLocalStorage() {
+    localStorage.removeItem("highScores");
+    listedHighScores.innerHTML = "";
 }
+
+clearButton.addEventListener("click", clearLocalStorage);
